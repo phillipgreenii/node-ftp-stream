fa679b9200695ac7a0951180ee2a6cc8db9433bd
Phillip Green II <phillip.green.ii@gmail.com>
2015-07-27 20:01:47 -0400

    add tests to check file content

--- test/index.test.js
+++ test/index.test.js
@@ -71,7 +71,7 @@ function createDrain() {
 
 
 
-test('stream should contain the correct amount of files (when  one file)', function (t) {
+test('stream should contain the correct amount of files (when one file)', function (t) {
   t.plan(2);
 
   var server = runFtpServer();
@@ -95,6 +95,29 @@ test('stream should contain the correct amount of files (when  one file)', funct
     done();
   })).on('error', done);
 
+});
+
+test('stream should contain the correct contents (when one file)', function (t) {
+  t.plan(2);
+
+  var server = runFtpServer();
+  function done(error) {
+    t.error(error);
+    server.completelyShutdown();
+    t.end();
+  }
+
+  var file = 'file0.txt';
+  ftpStream({port:2100}, file)
+  .pipe(through2.obj(function(file, enc, callback){
+    streamEqual(fs.createReadStream(path.join(__dirname, 'data', 'file0.txt')), file.contents, function(err, equal) {
+      t.ok(equal, "Streams for 'file0.txt' are not equal");
+      callback();
+    });
+  }, function(cb){
+    done();
+    cb();
+  })).on('error', done);
 });
 
 test('stream should contain the correct amount of files (when array of one file)', function (t) {
@@ -123,6 +146,29 @@ test('stream should contain the correct amount of files (when array of one file)
 
 });
 
+test('stream should contain the correct contents (when array of one file)', function (t) {
+  t.plan(2);
+
+  var server = runFtpServer();
+  function done(error) {
+    t.error(error);
+    server.completelyShutdown();
+    t.end();
+  }
+
+  var files = ['file0.txt'];
+  ftpStream({port:2100}, files)
+  .pipe(through2.obj(function(file, enc, callback){
+    streamEqual(fs.createReadStream(path.join(__dirname, 'data', 'file0.txt')), file.contents, function(err, equal) {
+      t.ok(equal, "Streams for 'file0.txt' are not equal");
+      callback();
+    });
+  }, function(cb){
+    done();
+    cb();
+  })).on('error', done);
+});
+
 test('stream should contain the correct amount of files (when array of multiple files)', function (t) {
   t.plan(2);
 
@@ -147,4 +193,37 @@ test('stream should contain the correct amount of files (when array of multiple 
     done();
   })).on('error', done);
 
+});
+
+test('stream should contain the correct contents (when array of multiple files)', function (t) {
+  t.plan(4);
+
+  var server = runFtpServer();
+  function done(error) {
+    t.error(error);
+    server.completelyShutdown();
+    t.end();
+  }
+
+  var files = ['/file0.txt', '/dir1/file1.txt', '/dir2/file2.txt'];
+  var counter = 0;
+  var f;
+  ftpStream({port:2100}, files)
+  .pipe(through2.obj(function(file, enc, callback){
+    if(counter == 0) {
+      f = path.join(__dirname, 'data', 'file0.txt');
+    } else if(counter == 1) {
+      f = path.join(__dirname, 'data', 'dir1', 'file1.txt');
+    } else if(counter == 2) {
+      f = path.join(__dirname, 'data', 'dir2', 'file2.txt');
+    }
+    streamEqual(fs.createReadStream(f), file.contents, function(err, equal) {
+      t.ok(equal, "Streams for '"+files[counter]+"' are not equal");
+      callback();
+    });
+    counter++;
+  }, function(cb){
+    done();
+    cb();
+  })).on('error', done);
 });
